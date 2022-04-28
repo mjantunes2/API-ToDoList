da0a8f5e54dd79eee021f4a5ce11b8e1d61bb0f8
mjantunes2 <mjantunes09@gmail.com>
2022-04-28 15:40:54 -0300

    :sparkles: feat - ToDoModel.js

---
+++ src/models/ToDoModel.js
@@ -0,0 +1,21 @@
+const sequelize = require("../database/connection.js");
+const { DataTypes } = require("sequelize");
+
+const ToDoModel = sequelize.define("ToDo", {
+  atividade: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+
+  descricao: {
+    type: DataTypes.STRING,
+    required: true,
+  },
+
+  feito: {
+    type: DataTypes.BOOLEAN,
+    required: true,
+  },
+});
+
+module.exports = ToDoModel;
